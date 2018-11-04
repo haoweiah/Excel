ede15ded701e1683db8e87db31bdf988a7e00933
haow <hw121298@163.com>
2018-11-04 13:59:55 +0800

    first commit

---
+++ excelparse.py
@@ -0,0 +1,19 @@
+# coding=utf-8
+
+from openpyxl import workbook,load_workbook
+
+
+def rdexcel():
+    wb = load_workbook(filename='report_20181101_193155.xlsx')
+    ws = wb.active
+    sheets = wb.sheetnames
+    sheet_first = sheets[0]
+    ws = wb[sheet_first]
+    rows = ws.rows
+    columns = ws.columns
+    print(ws['A1'].value)
+    wb.create_sheet('test', 0)
+    wb.save('test.xlsx')
+
+
+rdexcel()
